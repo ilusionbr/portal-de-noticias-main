8ebfa981412f5fb1818a4b4f93ad03774ac61d2d
Bruno <106693058+ilusionbr@users.noreply.github.com>
2024-09-14 05:53:00 -0300

    Update dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 
 var connMysql = function (){
 	return mysql.createConnection({
-      	host: 'localhost',
-      	user: 'root',
-      	password: '1234',
+      	host: 'mysql-29dbe785-projeto-inicial-aprendizado.c.aivencloud.com',
+      	user: 'avnadmin',
+      	password: 'AVNS_zJ5af5UGzmNfd7AbcqK',
       	database: 'portal_noticias',
       	insecureAuth: true
     });
