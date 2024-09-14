7b3b2f9dcbe713b9335c58732da723d9553e5888
Bruno <106693058+ilusionbr@users.noreply.github.com>
2024-09-14 05:14:17 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 var app = require('./config/server');
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(4000, function(){
+	console.log('servidor ON');
+});
